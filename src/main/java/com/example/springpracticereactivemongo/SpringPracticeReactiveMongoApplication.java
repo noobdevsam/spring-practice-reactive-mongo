a683ee21674b58f8d7407207b34b178926403dd3
noobdevsam <mdshoykotislam05@gmail.com>
2025-05-07 17:36:00 +0600

    initialize Spring Boot project with Maven wrapper and MongoDB configuration

---
+++ src/main/java/com/example/springpracticereactivemongo/SpringPracticeReactiveMongoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springpracticereactivemongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPracticeReactiveMongoApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPracticeReactiveMongoApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/example/springpracticereactivemongo/SpringPracticeReactiveMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springpracticereactivemongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeReactiveMongoApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
