8f93a724f010c51a61350fbe1343b99601db7281
noobdevsam <mdshoykotislam05@gmail.com>
2025-05-08 00:58:16 +0600

    implement createBeer method in BeerServiceImpl to save BeerDTO using BeerRepository and BeerMapper

--- src/main/java/com/example/springpracticereactivemongo/services/impl/BeerServiceImpl.java
+++ src/main/java/com/example/springpracticereactivemongo/services/impl/BeerServiceImpl.java
@@ -1,12 +1,25 @@
 package com.example.springpracticereactivemongo.services.impl;
 
+import com.example.springpracticereactivemongo.mappers.BeerMapper;
 import com.example.springpracticereactivemongo.model.BeerDTO;
+import com.example.springpracticereactivemongo.repositories.BeerRepository;
 import com.example.springpracticereactivemongo.services.BeerService;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 @Service
 public class BeerServiceImpl implements BeerService {
+	
+	private final BeerRepository beerRepository;
+	private final BeerMapper beerMapper;
+	
+	public BeerServiceImpl(
+		BeerRepository beerRepository,
+		BeerMapper beerMapper
+	) {
+		this.beerRepository = beerRepository;
+		this.beerMapper = beerMapper;
+	}
 	
 	@Override
 	public Mono<BeerDTO> getBeerById(String id) {
@@ -15,7 +28,10 @@ public class BeerServiceImpl implements BeerService {
 	
 	@Override
 	public Mono<BeerDTO> createBeer(Mono<BeerDTO> beerDTO) {
-		return null;
+		return beerDTO
+			       .map(beerMapper::beerDTOToBeer)
+			       .flatMap(beerRepository::save)
+			       .map(beerMapper::beerToBeerDTO);
 	}
 	
 }
