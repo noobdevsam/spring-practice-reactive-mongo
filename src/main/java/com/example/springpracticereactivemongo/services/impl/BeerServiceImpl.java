7c098f3bae8543843e7edcf8b742bf4215cc9d5b
noobdevsam <mdshoykotislam05@gmail.com>
2025-05-08 01:41:50 +0600

    implement findAll, saveBeer, updateBeer, patchBeer, and deleteBeerById methods in BeerServiceImpl

--- src/main/java/com/example/springpracticereactivemongo/services/impl/BeerServiceImpl.java
+++ src/main/java/com/example/springpracticereactivemongo/services/impl/BeerServiceImpl.java
@@ -1,10 +1,12 @@
 package com.example.springpracticereactivemongo.services.impl;
 
+import com.example.springpracticereactivemongo.domain.Beer;
 import com.example.springpracticereactivemongo.mappers.BeerMapper;
 import com.example.springpracticereactivemongo.model.BeerDTO;
 import com.example.springpracticereactivemongo.repositories.BeerRepository;
 import com.example.springpracticereactivemongo.services.BeerService;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -22,8 +24,15 @@ public class BeerServiceImpl implements BeerService {
 	}
 	
 	@Override
+	public Flux<BeerDTO> findAll() {
+		return beerRepository.findAll()
+			       .map(beerMapper::beerToBeerDTO);
+	}
+	
+	@Override
 	public Mono<BeerDTO> getBeerById(String id) {
-		return null;
+		return beerRepository.findById(id)
+			       .map(beerMapper::beerToBeerDTO);
 	}
 	
 	@Override
@@ -32,6 +41,54 @@ public class BeerServiceImpl implements BeerService {
 			       .map(beerMapper::beerDTOToBeer)
 			       .flatMap(beerRepository::save)
 			       .map(beerMapper::beerToBeerDTO);
+	}
+	
+	@Override
+	public Mono<BeerDTO> saveBeer(BeerDTO beerDTO) {
+		return beerRepository.save(beerMapper.beerDTOToBeer(beerDTO))
+			       .map(beerMapper::beerToBeerDTO);
+	}
+	
+	@Override
+	public Mono<BeerDTO> updateBeer(String id, BeerDTO beerDTO) {
+		return getBeerById(id)
+			       .flatMap(found_beer ->
+				                beerRepository.save(
+					                new Beer(
+						                found_beer.id(),
+						                beerDTO.beerName(),
+						                beerDTO.beerStyle(),
+						                beerDTO.upc(),
+						                beerDTO.quantityOnHand(),
+						                beerDTO.price(),
+						                found_beer.createdDate(),
+						                found_beer.lastModifiedDate()
+					                )
+				                ))
+			       .map(beerMapper::beerToBeerDTO);
+	}
+	
+	@Override
+	public Mono<BeerDTO> patchBeer(String id, BeerDTO beerDTO) {
+		return getBeerById(id)
+			       .flatMap(found_beer ->
+				                beerRepository.save(new Beer(
+						                found_beer.id(),
+						                beerDTO.beerName() != null ? beerDTO.beerName() : found_beer.beerName(),
+						                beerDTO.beerStyle() != null ? beerDTO.beerStyle() : found_beer.beerStyle(),
+						                beerDTO.upc() != null ? beerDTO.upc() : found_beer.upc(),
+						                beerDTO.quantityOnHand() != null ? beerDTO.quantityOnHand() : found_beer.quantityOnHand(),
+						                beerDTO.price() != null ? beerDTO.price() : found_beer.price(),
+						                found_beer.createdDate(),
+						                found_beer.lastModifiedDate()
+					                )
+				                ))
+			       .map(beerMapper::beerToBeerDTO);
+	}
+	
+	@Override
+	public Mono<Void> deleteBeerById(String id) {
+		return beerRepository.deleteById(id);
 	}
 	
 }
